7eeb189d4875a20e71c9ec751cce90d6dc626dc6
tantan-mkkim <tantan.mkkim@gmail.com>
2021-06-08 11:16:13 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+//body tag 에 해당 (웹사이트의 화면 출력)
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Lets develop management system!</h2>
     </div>
   );
 }
