25b1685c91b9af4bd4c063593908010351387e65
tantan-mkkim <tantan.mkkim@gmail.com>
2021-06-09 16:05:47 +0900

    Tutorial source code 적용 완료(my.api.url REST API 적용 제외)

--- src/App.js
+++ src/App.js
@@ -4,31 +4,26 @@
 import Customer from './components/Customer';
 import './App.css';
 //import { render } from '@testing-library/react';
-
-class App extends Component {
-  render() {
-    return (
-      <Customer/>
-    );
-  }
-}
-
-export default App; */
+*/
 
 //import React from 'react';
 import React from 'react';
-//import { Admin, Resource } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 //import { Admin, Resource, ListGuesser } from 'react-admin';
-import { Admin, Resource, EditGuesser } from 'react-admin';
+//import { Admin, Resource, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from './posts';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={EditGuesser} />        
-        <Resource name="users" list={UserList} />
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
--- src/posts.js
+++ src/posts.js
@@ -1,21 +1,34 @@
 // in src/posts.js
 import * as React from "react";
+import { useMediaQuery } from '@material-ui/core';
 //import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
 import {
+    Filter,
     List,
+    SimpleList,
     Datagrid,
     TextField,
     ReferenceField,
     EditButton,
     Edit,
+    Create,
     SimpleForm,
     ReferenceInput,
     SelectInput,
     TextInput,
 } from 'react-admin';
 
-export const PostList = props => (
-    <List {...props}>
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+/* export const PostList = props => (
+    <List filters={<PostFilter />} {...props}>
        <Datagrid>
            <TextField source="id" />
             <ReferenceField source="userId" reference="users">
@@ -25,11 +38,39 @@ export const PostList = props => (
            <EditButton />
         </Datagrid>
     </List>
-);
+); */
 
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField label="User" source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <TextField source="body" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>
+};
 
 export const PostEdit = props => (
-    <Edit {...props}>
+    <Edit title={<PostTitle />}{...props}>
         <SimpleForm>
             <TextInput disabled source="id" />
             <ReferenceInput source="userId" reference="users">
@@ -40,3 +81,15 @@ export const PostEdit = props => (
         </SimpleForm>
     </Edit>
 );
+
+export const PostCreate = props => (
+        <Create {...props}>
+           <SimpleForm>
+               <ReferenceInput source="userId" reference="users">
+                   <SelectInput optionText="name" />
+               </ReferenceInput>
+               <TextInput source="title" />
+               <TextInput multiline source="body" />
+           </SimpleForm>
+       </Create>
+    );
